da0bca1e56c92ac1e85523041b79d8ceb06088e8
“bbkscala17” <bbkscala17@users.noreply.github.com>
2017-02-04 17:54:51 +0000

    bnz initial commit not working

---
+++ coursework/cw-one/SML_Scala/src/sml/BnzInstruction.scala
@@ -0,0 +1,21 @@
+package sml
+
+class BnzInstruction(label: String, op: String, val result: Int, val op1: Int, val op2: Int)
+  extends Instruction(label, op) {
+
+  override def execute(m: Machine) {
+    val value1 = op1
+    val value2 = op2
+
+    m.regs(result) = value1 / value2
+  }
+
+  override def toString(): String = {
+    super.toString + " " + op1 + " / " + op2 + " to " + result
+  }
+}
+
+object BnzInstruction {
+  def apply(label: String, result: Int, op1: Int, op2: Int) =
+    new BnzInstruction(label, "div", result, op1, op2)
+}

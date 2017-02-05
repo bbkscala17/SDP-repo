93262134c1e84224bd9a875532e1132af80e1445
“bbkscala17” <bbkscala17@users.noreply.github.com>
2017-02-05 15:54:39 +0000

    BNZ fixed

--- coursework/cw-one/SML_Scala/src/sml/BnzInstruction.scala
+++ coursework/cw-one/SML_Scala/src/sml/BnzInstruction.scala
@@ -6,16 +6,13 @@ class BnzInstruction(label: String, op: String, val s1: Int, nextLabel : String)
   override def execute(m: Machine) {
     println("Starting BNZ - programme counter is " + m.pc.toString)
     val contentas1 = s1
-    println("In BNZ - here are the labels.")
-    println(m.labels)
     val labelsArray = m.labels.toString().split(" : ")
-    m.pc = labelsArray.indexOf(nextLabel);
+    m.pc = labelsArray.indexOf(nextLabel); // do not add 1 because, although PC starts at 1 and labelsArray starts at 0, counter incremented at end of this bnz instruction anyway
     println("Ending BNZ - programme counter is now " + m.pc.toString)
   }
 
   override def toString(): String = {
     super.toString + " " + s1 + " next to execute is " + nextLabel
-    //TODO finish
   }
 }
 

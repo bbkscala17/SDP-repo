03cc9b23306b9c5a73771905859c04897765c9eb
“bbkscala17” <bbkscala17@users.noreply.github.com>
2017-02-18 15:22:45 +0000

    word count map kind of working in worksheet

--- exercises/week03/src/main/scala/basics/ScalaBasics.scala
+++ exercises/week03/src/main/scala/basics/ScalaBasics.scala
@@ -228,6 +228,10 @@ object ScalaBasics {
    * @param lines the lines of a text file
    * @return a map from words to the number of times that word was seen
    */
-  def wordCounter(lines: Array[String]): Map[String, Int] = ???
+  def wordCounter(lines: Array[String]): Map[String, Int] = {
+    val wordSet = lines.toSet
+    val wordMap = wordSet.map(word => lines.count(_ == word))
+    wordMap
+  }
 
 }
--- exercises/week03/src/main/scala/basics/testworksheet.sc
+++ exercises/week03/src/main/scala/basics/testworksheet.sc
@@ -1,4 +1,9 @@
-
-
-
+var lines = Array("this", "is", "a", "string", "string")
+def countFunc(word: String, lines: Array[String]): Int = {
+  lines.count(_ == word)
 }
+println(countFunc("this", lines))
+
+var wordSet = lines.toSet
+var wordMap = wordSet.map(word => (word, countFunc(word, lines)))
+println(wordMap.toString)

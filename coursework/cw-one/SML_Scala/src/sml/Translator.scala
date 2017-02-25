af574563c8283cec44f51b2553ddea78eeec0a7e
bbkscala17 <bbkscala17@users.noreply.github.com>
2017-02-25 12:06:36 +0000

    tidy

--- coursework/cw-one/SML_Scala/src/sml/Translator.scala
+++ coursework/cw-one/SML_Scala/src/sml/Translator.scala
@@ -99,39 +99,3 @@ class Translator(fileName: String) {
 object Translator {
   def apply(file: String) = new Translator(file)
 }
-
-//import scala.io.StdIn._
-//
-//trait Instruction {
-//  def action(name: String*) : String
-//}
-//
-//trait Add extends Instruction
-//trait Sub extends Instruction
-//
-//class AddImpl extends Instruction {
-//  def action(name: String*): String = s"Executing an instruction $name"
-//}
-//
-//object FooMain extends App {
-//  print("Enter a class name: ")
-//  val name = readLine
-//  try {
-//    Class.forName(name)
-//    val className = name + "Impl"
-//    try {
-//      val actualClass = Class.forName(className)
-//      val foo = actualClass.newInstance.asInstanceOf[Instruction]
-//      println("foo is "  + foo)
-//      println(foo.action("Crash the machine"))
-//    }
-//    catch {
-//      case ex: ClassNotFoundException =>
-//        println(s"No implementation for [$name] found")
-//    }
-//  }
-//  catch {
-//    case ex: ClassNotFoundException =>
-//      println(s"No class of instruction [$name]")
-//  }
-//}

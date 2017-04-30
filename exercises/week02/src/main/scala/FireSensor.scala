b3ab8d062606dc010d61e59adbafc356a7e1ec78
Dev <scala@davidwatson.com>
2017-04-30 16:04:01 +0100

    fire sensor class

---
+++ exercises/week02/src/main/scala/FireSensor.scala
@@ -0,0 +1,11 @@
+class FireSensor extends Sensor {
+
+  def isTriggered: Boolean = false
+
+  def getLocation: String = "to implement"
+
+  def getSensortype: String = "to implement"
+
+  def getBatteryPercentage: Int = 0
+
+}

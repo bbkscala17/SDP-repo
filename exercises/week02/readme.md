365d9d03e279fab078a09dfccbd4722b31dc6ca2
Dev <scala@davidwatson.com>
2017-04-30 16:17:09 +0100

    add keiths initial pollsensors method

---
+++ exercises/week02/readme.md
@@ -0,0 +1,3 @@
+3. What are the current responsibilities of ControlUnit.pollSensors()?
+It must poll the sensors to see if any are triggered and raise the alarm if they are
+
--- exercises/week02/src/main/scala/ControlUnit.scala
+++ exercises/week02/src/main/scala/ControlUnit.scala
@@ -1,3 +1,22 @@
 class ControlUnit {
 
+  import scala.collection.mutable.ListBuffer
+
+  class ControlUnit {
+    def pollSensors() {
+      val sensors = new ListBuffer[Sensor]()
+      sensors += new FireSensor()
+      sensors += new SmokeSensor()
+      for (sensor <- sensors) {
+        if (sensor.isTriggered) {
+          System.out.println("A " + sensor.getSensorType + " sensor was triggered at " + sensor.getLocation)
+        }
+        else {
+          System.out.println("Polled " + sensor.getSensorType + " at " + sensor.getLocation + " successfully")
+        }
+      }
+    }
+  }
+
+  object ControlUnit
 }

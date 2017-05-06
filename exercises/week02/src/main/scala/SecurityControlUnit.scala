a386d7877c3d605daded9683b00e84a0b9e34f87
Dev <scala@davidwatson.com>
2017-05-06 21:01:55 +0100

    adding time check

--- exercises/week02/src/main/scala/SecurityControlUnit.scala
+++ exercises/week02/src/main/scala/SecurityControlUnit.scala
@@ -3,12 +3,12 @@ import java.util.Calendar
 class SecurityControlUnit extends ControlUnit {
   override def pollSensors(): Unit = {
     val now = Calendar.getInstance()
-    val currentHour = now.get(Calendar.HOUR)
-//    currentHour match{
-//      case <6 =>
-//    }
-//    println(currentMinute);
-//  }
+    val currentHour = now.get(Calendar.HOUR_OF_DAY)
+    if(currentHour < 6 || currentHour >= 22 ){
+      println("night")
+  } else {
+      println("day")
+    }
 
   //  private def timecheck(): Boolean ={
 }

8ede61a947ac2625c221d75a3f9d81557032c764
Dev <scala@davidwatson.com>
2017-05-09 11:00:26 +0100

    Q2b complete

---
+++ exercises/week05_classes/src/main/scala/PersonCase.scala
@@ -0,0 +1,12 @@
+case class PersonCase(name: Array[String]) {
+  println("Person created with name ...")
+  for(word<-name) println(word)
+
+  def getName(): Array[String] = name
+}
+
+object PersonCase{
+  def apply(name:String): PersonCase = {
+    new PersonCase(name.split(" "))
+  }
+}

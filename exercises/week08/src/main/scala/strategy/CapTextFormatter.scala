fe871819e751267c4480dca24759848f11d33605
Dev <scala@davidwatson.com>
2017-05-13 20:03:12 +0100

    Q1 strategy complete

--- exercises/week08/src/main/scala/strategy/CapTextFormatter.scala
+++ exercises/week08/src/main/scala/strategy/CapTextFormatter.scala
@@ -4,5 +4,7 @@ package strategy
 // here the 'format' method is the 'execute' method of that concrete strategy
 
 case class CapTextFormatter() extends TextFormatter {
-  override def format(text: String): Unit = text.toUpperCase
+  override def format(text: String): Unit = {
+    println("[" + this.getClass + "] " + text.toUpperCase())
+  }
 }
--- exercises/week08/src/main/scala/strategy/LowerTextFormatter.scala
+++ exercises/week08/src/main/scala/strategy/LowerTextFormatter.scala
@@ -2,6 +2,9 @@ package strategy
 
 // this class is a concrete strategy class and
 // here the 'format' method is the 'execute' method of that concrete strategy
+// as this returns nothing it must have to print the text to screen too
 case class LowerTextFormatter() extends TextFormatter {
-  override def format(text: String): Unit = text.toLowerCase()
+  override def format(text: String): Unit = {
+    println("[" + this.getClass + "] " + text.toLowerCase())
+  }
 }

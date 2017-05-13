39455d272bc2546687b1f711cdd3ecee46f00a77
Dev <scala@davidwatson.com>
2017-05-13 17:11:07 +0100

    move code for concrete subscriver class to existing commentary object class and concrete observer to smsuser class

--- exercises/week07/src/main/scala/observer/CommentaryObject.scala
+++ exercises/week07/src/main/scala/observer/CommentaryObject.scala
@@ -1,13 +1,15 @@
 package observer
+// this is the Concrete Subject class
+// it maintains the list of observers (here the 'subsribers' var passed in constuctor
 
 import scala.collection.mutable.ListBuffer
 
 class CommentaryObject(var subscribers: ListBuffer[Observer], val title: String) extends Subject {
-  def subscribeObserver(observer: Observer) = ???
+  def subscribeObserver(observer: Observer) = subscribers += observer
 
-  def unSubscribeObserver(observer: Observer) = ???
+  def unSubscribeObserver(observer: Observer) = subscribers -= observer
 
-  def notifyObservers() = ???
+  def notifyObservers() = for(observer <- subscribers) notify()
 
   def subjectDetails: String = ???
 }
--- exercises/week07/src/main/scala/observer/SMSUsers.scala
+++ exercises/week07/src/main/scala/observer/SMSUsers.scala
@@ -1,11 +1,14 @@
 package observer
+// this is the concrete observer class.  It needs to hold a reference to concrete subject
+// whcih here it gets in the constructor as s
+// Each observer registers with a concrete subject to receive updates
 
 case class SMSUsers(s: Subject, msg: String) extends Observer {
   def update(desc: String): Unit = {
     println("notifying user " + s + "with message" + msg)
   }
 
-  def subscribe() = ???
+  def subscribe() = s.subscribeObserver(this)
 
-  def unSubscribe() = ???
+  def unSubscribe() = s.subscribeObserver(this)
 }

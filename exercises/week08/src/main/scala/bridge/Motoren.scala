2bf1340dd05770d7e2dcc15d4db366af25971134
Dev <scala@davidwatson.com>
2017-05-14 10:19:29 +0100

    Q10 Bridge pattern complete

--- exercises/week08/src/main/scala/bridge/Motoren.scala
+++ exercises/week08/src/main/scala/bridge/Motoren.scala
@@ -3,9 +3,9 @@ package bridge
 // TODO
 
 case class Motoren(product: Product, s: String) extends Car(product, s) {
-  override def assemble: Unit = ???
+  override def assemble: Unit = println("Assembling " + product.productName + " for " + s)
 
-  override def produceProduct: Unit = ???
+  override def produceProduct: Unit = println("Modifying " + product.productName + " according to " + s)
 
-  override def printDetails: Unit = println(s + product.productName)
+  override def printDetails: Unit = println("Car: " + s + ", Product: " + product.productName)
 }

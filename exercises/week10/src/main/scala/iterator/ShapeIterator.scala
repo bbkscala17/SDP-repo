a5744f93e25a4fd4bd435b3aeebabdd177b35116
Dev <scala@davidwatson.com>
2017-05-14 17:10:37 +0100

    Q5 iterator done

--- exercises/week10/src/main/scala/iterator/ShapeIterator.scala
+++ exercises/week10/src/main/scala/iterator/ShapeIterator.scala
@@ -3,10 +3,19 @@ package iterator
 import java.util.Iterator
 
 class ShapeIterator(private var shapes: Array[Shape]) extends Iterator[Shape] {
+  var index = 0;
 
-  override def hasNext(): Boolean = ???
+  override def hasNext(): Boolean = index < shapes.size
 
-  override def next(): Shape = ???
+  override def next(): Shape = {
+    index +=1
+    shapes(index-1)
+  }
 
-  override def remove(): Unit = ???
+  /**
+    * remove the current item from the collection
+    */
+  override def remove(): Unit = {
+    shapes = shapes.drop(index)
+  }
 }
